88aadc8d94d2321c2feb75c3948092c7cc7ab958
rsegeda <rsegeda1992@gmail.com>
2014-12-27 01:28:14 +0100

    nic

--- res/draw_radar.js
+++ res/draw_radar.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 	
-
+	
 	function redraw_background() {
 
 		var canvas = document.getElementById('radar_canvas');
@@ -29,6 +29,36 @@ $( document ).ready(function() {
 		
 
 	}
+	
+	
+	// Ajax part
+	
+	
+	function assign_position()
+	{
+	var a;
+	 
+		if (window.XMLHttpRequest)
+		{// If the browser if IE7+[or]Firefox[or]Chrome[or]Opera[or]Safari
+			a=new XMLHttpRequest();
+		}
+		else
+		{//If browser is IE6, IE5
+			a=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+	 
+		a.onreadystatechange=function()
+		{
+		  if (a.readyState==4 && a.status==200)
+			{
+				document.getElementById("myDiv").innerHTML=a.responseText;
+			}
+		}
+		 
+		a.open("POST","java4s.txt",true);
+		a.send();
+	} // fun1() close
+	
 	function init() {
 		
 		redraw_background();
