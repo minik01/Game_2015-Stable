96d4c864e19c4c660f3a9ae0e6285ad8b64b5baf
rsegeda <rsegeda1992@gmail.com>
2014-12-29 16:57:55 +0100

    poprawiony timer

--- src/Timers.java
+++ src/Timers.java
@@ -1,7 +1,7 @@
 import java.util.Timer;
 
 
-public class Timers {
+public class Timers extends TimerTask {{
 	
 	private Utilities log; 
 	public DBManager2 dbm2;
@@ -10,26 +10,28 @@ public class Timers {
 	
 	public boolean ResetTurn(){
 		
-		try {
-			new java.util.Timer().schedule( 
-			        new java.util.TimerTask() {
-			            @Override
-			            public void run() {
-			                log.print("Turn reset now!")
-			                
-			                // TODO tutaj trzeba pobrac tabele ships z bazy danych i wyciagnac wartosci
-			                
-			                // TODO tutaj trzeba zapisac to do pliku xml 
-			                xmlman = new XmlShipsManagement("ships_data.xml","ship");
-			                
-			                
-			            }
-			        }, 
-			        30000 // [ms] = 30 s
-			);
+		try{
+			
+		}
+			Runnable reset = new Runnable() {
+			    public void run() {
+			    	
+			    	log.print("Turn reset now!")
+	                
+	                // TODO tutaj trzeba pobrac tabele ships z bazy danych i wyciagnac wartosci
+	                
+	                // TODO tutaj trzeba zapisac to do pliku xml 
+	                xmlman = new XmlShipsManagement("ships_data.xml","ship");
+			    }
+			};
+	
+			ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+			executor.scheduleAtFixedRate(reset, 0, 30, TimeUnit.SECONDS);
+		
 			return true; // true when calculations are over
 		}catch(ExecutionException ex) {
 		    log.print("Exception caught while reseting turn");
+		    return false;
 
 		}	
 		
